776ab871ba83fafd928556a89e6268037710e264
Jeff Gonzalez <jeff@hypertheory.com>
2026-03-11 14:42:33 -0400

    before break

--- src/SoftwareSolutionWeds/Vendors.Api/Vendors/Controller.cs
+++ src/SoftwareSolutionWeds/Vendors.Api/Vendors/Controller.cs
@@ -17,13 +17,26 @@ public class Controller(IDocumentSession session) : ControllerBase
     [FromBody] CreateVendorRequestModel request
     )
     {
-        return Ok();
+        // at this particular date and time a new vendor was created.
+        // and a point of contact was assigned to that vendor
+        // streamId "the aggregate" - everything that happens to a specific vendor will be correlated by a streamid.
+        var vendorId = Guid.NewGuid();
+        session.Events.StartStream(vendorId,
+            new VendorCreated(request.Name, request.Url),
+            new PointOfContactAssignedToAVendor(request.PointOfContact.Name, request.PointOfContact.Email, request.PointOfContact.Phone)
+            );
+        await session.SaveChangesAsync();
+        return Created($"/vendors/{vendorId}", new { });
     }
 
    
     [HttpPut("/vendors/{id:guid}/point-of-contact")]
     public async Task<ActionResult> UpdatePoc(Guid id, [FromBody] VendorPointOfContactModel request)
     {
+        // probably should check to see if that id is a real vendor...
+
+        session.Events.Append(id, new PointOfContactAssignedToAVendor(request.Name, request.Email, request.Phone));
+        await session.SaveChangesAsync();
         return Ok();
     }
    
@@ -36,6 +49,15 @@ public class Controller(IDocumentSession session) : ControllerBase
     [HttpGet("/vendors/{id:guid}")]
     public async Task<ActionResult> GetVendorByIdAsync(Guid id, CancellationToken token)
     {
-        return Ok();
+        // this is is saying RIGHT NOW go through all the events in this system, one by one, and construct this model.
+        // this will be 100% accurate with the state of the events. 
+        var vendor = await session.Events.AggregateStreamAsync<VendorDetailsReadModel>(id);
+        if (vendor == null)
+            return NotFound();
+
+        return Ok(vendor);
     }
 }
+
+public record VendorCreated(string Name, string Url);
+public record PointOfContactAssignedToAVendor(string Name, string Email, string Phone);
